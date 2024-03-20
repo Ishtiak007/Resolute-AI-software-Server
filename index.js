b955f31b3984b0fa4034a0b5ea7115df76efd93d
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-03-21 05:31:01 +0600

    Delete operation done with tanstack query

--- index.js
+++ index.js
@@ -49,6 +49,12 @@ async function run() {
         const result = await usersCollection.find().toArray();
         res.send(result);
     });
+    app.delete('/users/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await usersCollection.deleteOne(query);
+        res.send(result);
+    });
     
 
 
