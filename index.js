aebbd03b903ba638db2820e624919b12bf06a1e1
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-03-21 15:57:55 +0600

    user delete functionality implemented

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1bw0ez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -55,7 +55,7 @@ async function run() {
         const result = await usersCollection.deleteOne(query);
         res.send(result);
     });
-    
+     
 
 
 
